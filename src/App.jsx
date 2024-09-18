e6c8e1648e210348a7cb31447a38eb84b6453839
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-19 03:02:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import LandingPage from './LandingPage';
 import KNZPage from './KNZPage';
 import Profile from './list';
 import LinktreePage from "./LinkTree";
-import Post from './Post';
+
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -107,7 +107,7 @@ function App() {
               <Route path="/list" element={<Profile />} />
               <Route path="/knz" element={<KNZPage />} />
               <Route path="/link" element={<LinktreePage />} />
-              <Route path="/post/:id" element={<Post />} />
+              
             </Routes>
           </div>
 
