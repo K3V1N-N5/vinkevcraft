7274cf480144e16fdd024078a561522ab536a985
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-19 00:01:03 +0700

    Update list.jsx

--- src/list.jsx
+++ src/list.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import {data} from './utils/listdata';
+import { data } from './utils/listdata';
 
 function ListProject() {
   const navigate = useNavigate();
@@ -55,82 +55,84 @@ function ListProject() {
 
   return (
     <div id="landing" className="bg-white dark:bg-[#1e1e1e] text-black dark:text-white">
-    <div className="pt-14 pb-10 px-4 font-sans mx-auto relative z-10 w-full h-full max-w-3xl"> 
-      <div className="flex mt-10 max-w-4xl justify-between items-center mb-5 border-b border-gray-700 pb-2"> 
-        <button className="px-4 py-2 bg-gray-100 dark:bg-gray-800 rounded" onClick={() => navigate('/')}>Back</button>
-        
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={handleSearch}
-          className="px-2 py-1 w-full mx-2 bg-gray-100 dark:bg-gray-900 border border-gray-700 rounded text-white"
-        />
-        <button className="px-4 py-2 bg-gray-100 dark:bg-gray-800 rounded" onClick={toggleFilter}>Filter</button>
-      </div>
-
-      {filterVisible && (
-        <div className="flex flex-col absolute top-25 right-4 bg-gray-100 dark:bg-gray-800 p-2 rounded shadow-md z-20" ref={dropdownRef}>
-          <label>
-            <input
-              type="radio"
-              name="category"
-              checked={selectedCategory === 'All'}
-              onChange={() => handleFilterChange('All')}
-            />
-            All
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="category"
-              checked={selectedCategory === 'Resource Pack'}
-              onChange={() => handleFilterChange('Resource Pack')}
-            />
-            Resource Pack
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="category"
-              checked={selectedCategory === 'Addon'}
-              onChange={() => handleFilterChange('Addon')}
-            />
-            Addon
-          </label>
+      <div className="pb-10 px-4 font-sans mx-auto relative z-10 w-full h-full max-w-3xl"> 
+        {/* Navigasi dan Search */}
+        <div className="flex mt-10 max-w-4xl justify-between items-center mb-5 border-b border-gray-700 pb-2"> 
+          <button className="px-4 py-2 bg-gray-100 dark:bg-gray-800 rounded" onClick={() => navigate('/')}>Back</button>
+          
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleSearch}
+            className="px-2 py-1 w-full mx-2 bg-gray-100 dark:bg-gray-900 border border-gray-700 rounded text-white"
+          />
+          <button className="px-4 py-2 bg-gray-100 dark:bg-gray-800 rounded" onClick={toggleFilter}>Filter</button>
         </div>
-      )}
 
-      {/* Loading Spinner */}
-    {isLoading ? (
-      <div className="fixed inset-0 bg-gray-200 dark:bg-black bg-opacity-70 flex justify-center items-center z-30">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-white"></div>
+        {/* Dropdown Filter */}
+        {filterVisible && (
+          <div className="flex flex-col absolute top-25 right-4 bg-gray-100 dark:bg-gray-800 p-2 rounded shadow-md z-20" ref={dropdownRef}>
+            <label>
+              <input
+                type="radio"
+                name="category"
+                checked={selectedCategory === 'All'}
+                onChange={() => handleFilterChange('All')}
+              />
+              All
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="category"
+                checked={selectedCategory === 'Resource Pack'}
+                onChange={() => handleFilterChange('Resource Pack')}
+              />
+              Resource Pack
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="category"
+                checked={selectedCategory === 'Addon'}
+                onChange={() => handleFilterChange('Addon')}
+              />
+              Addon
+            </label>
+          </div>
+        )}
+
+        {/* Loading Spinner */}
+        {isLoading ? (
+          <div className="fixed inset-0 bg-gray-200 dark:bg-black bg-opacity-70 flex justify-center items-center z-30">
+            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-white"></div>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 mt-8"> {/* Grid Layout */}
+            {filteredData.map((item, index) => (
+              <Link
+                key={index}
+                to={item.link}
+                className="block bg-gray-100 dark:bg-gray-900 rounded-lg overflow-hidden shadow-md border border-gray-700 relative z-10"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="relative">
+                  <img src={item.img} alt={item.title} className="w-full h-auto object-cover border-b border-gray-700" />
+                  <div className="absolute top-2 left-2 bg-black bg-opacity-70 text-white px-2 py-1 rounded text-xs">{item.category}</div>
+                </div>
+                <div className="p-4 border-t border-gray-700">
+                  <h3 className="text-xl font-bold">{item.title}</h3>
+                  <p className="text-gray-500">{item.description}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
-    ) : (
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 mt-8"> {/* Grid untuk mengatur tata letak */}
-        {filteredData.map((item, index) => (
-          <Link
-            key={index}
-            to={item.link}
-            className="block bg-gray-100 dark:bg-gray-900 rounded-lg overflow-hidden shadow-md border border-gray-700 relative z-10" // Hapus flex-col dan atur padding di dalam
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="relative">
-              <img src={item.img} alt={item.title} className="w-full h-auto object-cover border-b border-gray-700" /> {/* Atur tinggi gambar */}
-              <div className="absolute top-2 left-2 bg-black bg-opacity-70 text-white px-2 py-1 rounded text-xs">{item.category}</div>
-            </div>
-            <div className="p-4 border-t border-gray-700"> {/* Pindahkan padding ke sini */}
-              <h3 className="text-xl font-bold">{item.title}</h3>
-              <p className="text-gray-500">{item.description}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-    )}
     </div>
-  </div>
   );
-};
+}
 
 export default ListProject;
