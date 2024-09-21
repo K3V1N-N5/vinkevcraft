71fd3d742d8a60f64987b351bef1ce7ddf53b87a
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-22 01:43:57 +0700

    Update Loading.jsx

--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Loading = ({ isDarkMode }) => {
   return (
-    <div className={`fixed inset-0 ${isDarkMode ? 'dark:bg-black' : 'bg-gray-200'} bg-opacity-70 flex justify-center items-center z-30`}>
-      <div className={`animate-spin rounded-full h-32 w-32 border-b-2 ${isDarkMode ? 'dark:border-white' : 'border-gray-900'}`}></div>
+    <div
+      className={`fixed inset-0 bg-gray-200 ${
+        isDarkMode ? 'dark:bg-black' : 'bg-white'
+      } bg-opacity-70 flex justify-center items-center z-30`}
+    >
+      <div
+        className={`animate-spin rounded-full h-32 w-32 border-b-2 ${
+          isDarkMode ? 'dark:border-white' : 'border-gray-900'
+        }`}
+      ></div>
     </div>
   );
 };
