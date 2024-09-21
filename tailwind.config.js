8cf900a38ca39bf7d0be241095a7a58f265053c0
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-22 01:19:14 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
-import { content as flowbiteContent, plugin as flowbitePlugin } from "flowbite-react/tailwind";
+import { content as _content, plugin } from "flowbite-react/tailwind";
 
 /** @type {import('tailwindcss').Config} */
 export const content = [
   "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",
-  ...flowbiteContent(), // gunakan spread operator untuk menyebarkan hasil dari flowbiteContent
+  _content(),
 ];
-
 export const theme = {
   extend: {},
 };
-
-export const darkMode = 'class'; // tambahkan dark mode support dengan menggunakan class
-
 export const plugins = [
-  flowbitePlugin(), // gunakan plugin dari flowbite-react untuk mendukung komponen Flowbite
+  plugin(),
 ];
