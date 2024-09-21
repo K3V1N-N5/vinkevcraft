fdd513f2cadad4a6ee6d77475f135ab878518632
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-21 19:56:17 +0700

    Update Loading.jsx

--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 bg-gray-200 dark:bg-black bg-opacity-70 flex justify-center items-center z-30">
+    <div className="fixed inset-0 bg-gray-200 dark:bg-black bg-opacity-70 dark:bg-opacity-70 flex justify-center items-center z-30">
       <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-white"></div>
     </div>
   );
