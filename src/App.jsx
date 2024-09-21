017eb27cad9a48e363d1d3ed95c2c12da76bc53f
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-21 10:57:53 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -123,10 +123,7 @@ function App() {
               </div>
               <Footer.Divider />
 
-              <div className="mt-2 text-sm text-gray-500 dark:text-gray-400">
-                <Footer.Copyright href="#" by="VinKev Craft" year={2024} />
-                All rights reserved.
-              </div>
+              <Footer.Copyright href="#" by="VinKev Craft.  All rights reserved." year={2024} />
 
               <p className="text-sm text-gray-500 dark:text-gray-400 mt-1">
                 This site is not affiliated with Mojang Studios.
