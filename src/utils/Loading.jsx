dc77bb7d3e3aab9c008dcb6cc6a7dd3e32176c8b
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-22 02:03:39 +0700

    Update Loading.jsx

--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Loading = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Ambil preferensi tema dari localStorage
+    const savedTheme = JSON.parse(localStorage.getItem('isDarkMode'));
+    if (savedTheme !== null) {
+      setIsDarkMode(savedTheme);
+    }
+  }, []);
+
   return (
-    <div className="fixed inset-0 bg-gray-200 dark:bg-black bg-opacity-70 dark:bg-opacity-70 flex justify-center items-center z-30">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-white"></div>
+    <div
+      className={`fixed inset-0 ${
+        isDarkMode ? 'bg-black bg-opacity-70' : 'bg-gray-200 bg-opacity-70'
+      } flex justify-center items-center z-30`}
+    >
+      <div
+        className={`animate-spin rounded-full h-32 w-32 border-b-2 ${
+          isDarkMode ? 'border-white' : 'border-gray-900'
+        }`}
+      ></div>
     </div>
   );
 };
