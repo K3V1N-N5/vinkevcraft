d8a9d6e74cb346da5f539ddb9538e9844b957d91
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-22 02:53:31 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import vinkev from './assets/vinkev_1.png'; // Ganti dengan path gambar logo yang benar
 import { Footer, DarkThemeToggle, Flowbite, Drawer, Sidebar } from "flowbite-react";
 import { useState, useEffect, Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { HiMenu, HiX, HiOutlineCollection, HiOutlineExternalLink, HiInformationCircle } from "react-icons/hi";
 import Loading from './utils/Loading'; // Import komponen Loading
 
@@ -17,6 +17,8 @@ function App() {
   const [isDarkMode, setIsDarkMode] = useState(true); // Untuk Dark Mode
   const [loading, setLoading] = useState(true); // Untuk memantau loading halaman
 
+  const location = useLocation(); // Gunakan untuk mendeteksi perubahan rute (halaman)
+
   // Check local storage for theme preference
   useEffect(() => {
     const savedTheme = JSON.parse(localStorage.getItem('isDarkMode')); 
@@ -30,14 +32,15 @@ function App() {
     localStorage.setItem('isDarkMode', JSON.stringify(isDarkMode)); 
   }, [isDarkMode]);
 
-  // Mengatur loading screen saat aplikasi pertama kali di-refresh
+  // Mengatur loading screen saat aplikasi pertama kali di-refresh dan setiap kali halaman berubah
   useEffect(() => {
+    setLoading(true); // Tampilkan loading saat halaman berubah
     const timer = setTimeout(() => {
       setLoading(false); // Setelah beberapa detik, set loading ke false
     }, 1000); // Set waktu loading selama 1 detik (sesuaikan sesuai kebutuhan)
     
-    return () => clearTimeout(timer); // Bersihkan timer saat komponen unmount
-  }, []);
+    return () => clearTimeout(timer); // Bersihkan timer saat komponen unmount atau saat rute berubah
+  }, [location]); // Gunakan useEffect saat lokasi berubah (halaman baru)
 
   const toggleDrawer = () => {
     setIsOpen(!isOpen); // Toggle untuk membuka/tutup Drawer (Sidebar)
