ffd4caa0826033d2de7838df32fdbf5b12a590fb
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-22 10:14:29 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import vinkev from './assets/vinkev_1.png'; // Ganti dengan path gambar logo yang benar
 import { Footer, DarkThemeToggle, Flowbite, Drawer, Sidebar } from "flowbite-react";
 import { useState, useEffect, Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { HiMenu, HiX, HiOutlineCollection, HiOutlineExternalLink, HiInformationCircle } from "react-icons/hi";
 import Loading from './utils/Loading'; // Import komponen Loading
 
@@ -16,8 +16,6 @@ function App() {
   const [isOpen, setIsOpen] = useState(false); // Untuk Drawer (Sidebar)
   const [isDarkMode, setIsDarkMode] = useState(true); // Untuk Dark Mode
   const [loading, setLoading] = useState(true); // Untuk memantau loading halaman
-
-  const location = useLocation(); // Untuk memantau perubahan rute
 
   // Check local storage for theme preference
   useEffect(() => {
@@ -41,16 +39,6 @@ function App() {
     return () => clearTimeout(timer); // Bersihkan timer saat komponen unmount
   }, []);
 
-  // Mengatur loading screen saat rute berubah
-  useEffect(() => {
-    setLoading(true); // Set loading menjadi true saat rute berubah
-    const timer = setTimeout(() => {
-      setLoading(false); // Setelah beberapa detik, set loading ke false
-    }, 500); // Set waktu loading selama 500ms (sesuaikan sesuai kebutuhan)
-
-    return () => clearTimeout(timer); // Bersihkan timer saat komponen unmount
-  }, [location.pathname]);
-
   const toggleDrawer = () => {
     setIsOpen(!isOpen); // Toggle untuk membuka/tutup Drawer (Sidebar)
   };
@@ -63,6 +51,8 @@ function App() {
     if (isOpen) {
       closeDrawer(); // Menutup Drawer ketika klik pada salah satu link
     }
+    setLoading(true); // Set loading ke true saat pindah halaman
+    setTimeout(() => setLoading(false), 1000); // Reset loading setelah 1 detik
   };
 
   if (loading) {
