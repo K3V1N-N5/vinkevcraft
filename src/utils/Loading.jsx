61291ae90cd3d99b44b5efc94ddccbfc5e70cffd
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-22 10:20:03 +0700

    Update Loading.jsx

--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -15,12 +15,12 @@ const Loading = () => {
     <div
       className={`fixed inset-0 ${
         isDarkMode ? 'bg-black bg-opacity-70' : 'bg-gray-200 bg-opacity-70'
-      } flex justify-center items-center z-30`}
+      } flex justify-center items-center z-30`} // Sama seperti yang digunakan di ListProject
     >
       <div
         className={`animate-spin rounded-full h-32 w-32 border-b-2 ${
           isDarkMode ? 'border-white' : 'border-gray-900'
-        }`}
+        }`} // Desain dan warna spinner sama seperti di ListProject
       ></div>
     </div>
   );
