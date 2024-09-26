740c48b11398def98c2fac5318f67a4427608cf7
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-09-26 08:36:41 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Ganti dengan file CSS yang kamu gunakan
+import App from './App';
+import { ThemeProvider } from './ThemeContext';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
