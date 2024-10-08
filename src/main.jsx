89ea39330bd73e85029ecab00c7e23acaa2d0334
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-10-08 14:13:32 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Ganti dengan file CSS yang kamu gunakan
 import App from './App';
 import { ThemeProvider } from './ThemeContext';
+
+// Cek apakah tema tersimpan di localStorage
+const savedTheme = localStorage.getItem('theme');
+
+// Jika ada tema tersimpan, tambahkan kelas 'dark' ke elemen <html>
+if (savedTheme) {
+  document.documentElement.classList.add(savedTheme);
+} else {
+  // Jika tidak ada, default ke dark mode dan simpan di localStorage
+  localStorage.setItem('theme', 'dark');
+  document.documentElement.classList.add('dark');
+}
 
 ReactDOM.render(
   <React.StrictMode>
