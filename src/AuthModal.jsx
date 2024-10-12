68088e940c24b7f0beb63f583a914e71ed368808
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-10-13 00:34:12 +0700

    Update AuthModal.jsx

--- src/AuthModal.jsx
+++ src/AuthModal.jsx
@@ -59,8 +59,10 @@ function AuthModal({ isModalOpen, toggleModal, setIsAdmin }) {
       // Simpan username ke Firestore
       await setDoc(doc(db, "usernames", username), { email });
 
-      // Tidak perlu cek admin saat register
-      setIsAdmin(false); // Set status admin ke false setelah register
+      // Cek apakah user adalah admin di Firestore
+      const adminDoc = await getDoc(doc(db, "admins", user.uid));
+      const isAdmin = adminDoc.exists(); // Jika dokumen ada, berarti user adalah admin
+      setIsAdmin(isAdmin); // Set status admin di state
 
       toggleModal(); // Tutup modal setelah berhasil register
     } catch (error) {
