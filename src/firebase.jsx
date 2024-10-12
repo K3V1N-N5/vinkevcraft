9952e51373cc42d7262d9593a54547a897ec5e3d
K3V1N <52075293+K3V1N-N5@users.noreply.github.com>
2024-10-12 23:12:35 +0700

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,1 +1,32 @@
 
+// Import Firebase SDK functions
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // Firestore import
+import { getStorage } from "firebase/storage"; // Import Firebase Storage
+import { getAuth } from "firebase/auth"; // Import Firebase Authentication
+
+// Firebase configuration object
+const firebaseConfig = {
+  apiKey: "AIzaSyAOX35VoU6pa_LE63KpQT16I7-lW0wTzmA",
+  authDomain: "vinkev-craft.firebaseapp.com",
+  projectId: "vinkev-craft",
+  storageBucket: "vinkev-craft.appspot.com",
+  messagingSenderId: "253877673444",
+  appId: "1:253877673444:web:2dc71388340933851d7bf1",
+  measurementId: "G-8Y7WNQ0GMK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore (database)
+const db = getFirestore(app);
+
+// Initialize Firebase Storage
+const storage = getStorage(app);
+
+// Initialize Firebase Auth
+const auth = getAuth(app);
+
+// Export Firestore, Storage, and Auth
+export { db, storage, auth };
